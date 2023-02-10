c59037f8747f96c21249a9079da42a48794efdbe
javeedsocial <78414554+javeedsocial@users.noreply.github.com>
2023-02-11 00:27:00 +0530

    a

---
+++ 7demo loop for each/main.tf
@@ -0,0 +1,12 @@
+resource "null_resource" "students" {
+  
+  for_each = {
+    student1 = "reem"
+    student2="raam"
+    student3="guy"
+  }
+  output student_names {
+    value=null_resource.students
+  }
+    
+}
